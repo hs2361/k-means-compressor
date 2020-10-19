1b304fbb13fc6381c58be611c368ff89b8778a8d
Harsh Sharma <sharmaharsh@me.com>
2020-10-19 19:40:35 +0530

    fixed formatting and memory errors

--- ImageRead.hpp
+++ ImageRead.hpp
@@ -3,45 +3,48 @@
 #include "stb_image.h"
 using namespace std;
 
-class ImageReader{
+class ImageReader
+{
 
-    private:
-    char* filename;
+private:
+    char *filename = NULL;
     int height, width, bpp;
     unsigned int channels;
 
-    public:
-    int ** matrix;
-    ImageReader(char*, unsigned int);
-    int length(){ return height*width;}
-    int getChannels() {return channels;}
+public:
+    int **matrix = NULL;
+    ImageReader(char *, unsigned int);
+    int length() { return height * width; }
+    int getChannels() { return channels; }
     ~ImageReader();
 };
 
-ImageReader::ImageReader(char* filename, unsigned int channels) {
+ImageReader::ImageReader(char *filename, unsigned int channels)
+{
 
-    uint8_t* image = stbi_load(filename, &this->width, &this->height, &this->bpp, channels);
+    uint8_t *image = stbi_load(filename, &this->width, &this->height, &this->bpp, channels);
     this->filename = filename;
     this->channels = channels;
 
-    matrix = new int*[height*width];
-    for(int i = 0; i < height*width; i++)
+    matrix = new int *[height * width];
+    for (int i = 0; i < height * width; i++)
         matrix[i] = new int[channels];
-    
-    for(int i = 0; i < height*width*channels; i+=channels) {
-        uint8_t* pixel = image + i;
-        for(int j = 0; j < channels; j++){
-            matrix[i/channels][j] = pixel[j];
+
+    for (int i = 0; i < height * width * channels; i += channels)
+    {
+        uint8_t *pixel = image + i;
+        for (int j = 0; j < channels; j++)
+        {
+            matrix[i / channels][j] = pixel[j];
         }
     }
     stbi_image_free(image);
 }
 
-ImageReader::~ImageReader() {
+ImageReader::~ImageReader()
+{
 
-    for(int i = 0; i < height*width; i++)
+    for (int i = 0; i < height * width; i++)
         delete[] matrix[i];
     delete[] matrix;
-
-    delete filename;
 }
--- ImageWrite.hpp
+++ ImageWrite.hpp
@@ -3,22 +3,23 @@
 #include "stb_image_write.h"
 using namespace std;
 
-class ImageWriter {
-    
-    private:
-    char* filename;
-    int width, height;
-    unsigned int channels;
+class ImageWriter
+{
+
+private:
+    char *filename = NULL;
+    int width = 0, height = 0;
+    unsigned int channels = 0;
     string getExtension();
 
-    public:
-    int** matrix;
-    ImageWriter(char*, int, int, unsigned int, int**);
+public:
+    int **matrix = NULL;
+    ImageWriter(char *, int, int, unsigned int, int **);
     void save();
-    ~ImageWriter();
 };
 
-ImageWriter::ImageWriter(char* filename, int width, int height, unsigned int channels, int** matrix) {
+ImageWriter::ImageWriter(char *filename, int width, int height, unsigned int channels, int **matrix)
+{
 
     this->filename = filename;
     this->width = width;
@@ -27,47 +28,43 @@ ImageWriter::ImageWriter(char* filename, int width, int height, unsigned int cha
     this->matrix = matrix;
 }
 
-string ImageWriter::getExtension() {
+string ImageWriter::getExtension()
+{
     string n = filename;
     int i;
-    for(i = n.length() - 1; i>=0 ; i--) {
-        if(n[i] == '.') break;
+    for (i = n.length() - 1; i >= 0; i--)
+    {
+        if (n[i] == '.')
+            break;
     }
     string ret;
-    ret.push_back(n[i+1]);
-    ret.push_back(n[i+2]);
-    ret.push_back(n[i+3]);
+    ret.push_back(n[i + 1]);
+    ret.push_back(n[i + 2]);
+    ret.push_back(n[i + 3]);
     return ret;
 }
 
-void ImageWriter::save() {
+void ImageWriter::save()
+{
 
     string extension = getExtension();
 
-    uint8_t* image = new uint8_t[height*width*channels];
-    for(int i = 0; i<height*width; i++) {
-        for(int j=0;j<channels;j++) {
-            image[(i*channels) + j] = matrix[i][j];
+    uint8_t *image = new uint8_t[height * width * channels];
+    for (int i = 0; i < height * width; i++)
+    {
+        for (int j = 0; j < channels; j++)
+        {
+            image[(i * channels) + j] = matrix[i][j];
         }
     }
-    if(extension == "png")  
-        stbi_write_png(filename, width, height, channels, image, width*channels);
-    else if(extension == "jpg")
+    if (extension == "png")
+        stbi_write_png(filename, width, height, channels, image, width * channels);
+    else if (extension == "jpg")
         stbi_write_jpg(filename, width, height, channels, image, 100);
-    else if(extension == "bmp")
+    else if (extension == "bmp")
         stbi_write_bmp(filename, width, height, channels, image);
-    else if(extension == "tga")
-        stbi_write_tga(filename, width, height, channels, image);  
-    else if(extension == "tga")
-        stbi_write_tga(filename, width, height, channels, image);  
+    else if (extension == "tga")
+        stbi_write_tga(filename, width, height, channels, image);
 
     delete[] image;
-}
-
-ImageWriter::~ImageWriter() {
-
-    delete filename;
-    for(int i = 0; i < height*width; i++)
-        delete[] matrix[i];
-    delete[] matrix;
 }
