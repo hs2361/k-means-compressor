67a4e2cbd20ce8f474ab9bb5047df46d02627d8e
Harsh Sharma <sharmaharsh@me.com>
2020-10-20 17:17:17 +0530

    fixed formatting

--- KMeans.hpp
+++ KMeans.hpp
@@ -4,18 +4,19 @@
 #include <time.h>
 using namespace std;
 
-class KMeans {
+class KMeans
+{
 
-    private:
+private:
     int height, width;
     unsigned int channels;
     int clusters;
-    int** centroids;
-    int* pixel_cluster_mapping;
+    int **centroids = NULL;
+    int *pixel_cluster_mapping = NULL;
 
-    public:
-    int** matrix;
-    KMeans(int, int**, int, int, unsigned int);
+public:
+    int **matrix = NULL;
+    KMeans(int, int **, int, int, unsigned int);
     void showClusters();
     void computeClosestCentroid();
     void computeCentroids();
@@ -23,53 +24,63 @@ class KMeans {
     ~KMeans();
 };
 
-KMeans::KMeans(int clusters, int** matrix, int height, int width, unsigned int channels) {
+KMeans::KMeans(int clusters, int **matrix, int height, int width, unsigned int channels)
+{
 
     int seed = time(0);
     srand(seed);
-    
+
     this->clusters = clusters;
     this->matrix = matrix;
     this->height = height;
     this->width = width;
     this->channels = channels;
 
-    centroids = new int* [clusters];
-    for(int i=0; i<clusters;i++) {
+    centroids = new int *[clusters];
+    for (int i = 0; i < clusters; i++)
+    {
         centroids[i] = new int[channels];
     }
 
-    for(int i=0; i<clusters; i++) {
-        int random = rand() % (height*width);
-        for(int j=0; j<channels; j++)
+    for (int i = 0; i < clusters; i++)
+    {
+        int random = rand() % (height * width);
+        for (int j = 0; j < channels; j++)
             centroids[i][j] = matrix[random][j];
     }
 
-    pixel_cluster_mapping = new int[height*width];
-    for(int i=0; i<height*width; i++)
+    pixel_cluster_mapping = new int[height * width];
+    for (int i = 0; i < height * width; i++)
         pixel_cluster_mapping[i] = rand() % clusters;
 }
 
-void KMeans::showClusters() {
-    for(int i=0; i<clusters; i++) {
-        for(int j=0; j<channels; j++)
-            cout<<centroids[i][j]<<" ";
-        cout<<endl;
+void KMeans::showClusters()
+{
+    for (int i = 0; i < clusters; i++)
+    {
+        for (int j = 0; j < channels; j++)
+            cout << centroids[i][j] << " ";
+        cout << endl;
     }
 }
 
-void KMeans::computeClosestCentroid() {
+void KMeans::computeClosestCentroid()
+{
 
-    for(int i=0; i<height*width; i++) {
+    for (int i = 0; i < height * width; i++)
+    {
         int closestCentroid = pixel_cluster_mapping[i];
         float min_distance = FLT_MAX;
         float distance = min_distance;
-        for(int j=0; j<clusters; j++) {
-            for(int k=0; k<channels; k++) {
+        for (int j = 0; j < clusters; j++)
+        {
+            for (int k = 0; k < channels; k++)
+            {
                 distance += pow((matrix[i][k] - centroids[j][k]), 2);
             }
             distance = sqrt(distance);
-            if(distance < min_distance) {
+            if (distance < min_distance)
+            {
                 min_distance = distance;
                 closestCentroid = j;
             }
@@ -78,41 +89,52 @@ void KMeans::computeClosestCentroid() {
     }
 }
 
-void KMeans::computeCentroids() {
-    
-    for(int i=0; i<clusters; i++) {
-        for(int j=0; j<channels; j++) {
+void KMeans::computeCentroids()
+{
+
+    for (int i = 0; i < clusters; i++)
+    {
+        for (int j = 0; j < channels; j++)
+        {
             int newCoordinate = 0;
             int frequency = 0;
-            for(int k=0;k <height*width; k++) {
-                if(pixel_cluster_mapping[k] == i) {
+            for (int k = 0; k < height * width; k++)
+            {
+                if (pixel_cluster_mapping[k] == i)
+                {
                     frequency++;
                     newCoordinate += matrix[k][j];
                 }
             }
-            if(frequency != 0) { 
+            if (frequency != 0)
+            {
                 newCoordinate = newCoordinate / frequency;
                 centroids[i][j] = newCoordinate;
             }
         }
     }
-    
 }
 
-void KMeans::assignCentroids() {
-    for(int i=0; i<height*width; i++) {
-        for(int j=0; j<clusters; j++) {
-            if(pixel_cluster_mapping[i] == j)
-            for(int k=0; k<channels; k++) {
-                matrix[i][k] = centroids[j][k];
-            }
+void KMeans::assignCentroids()
+{
+    for (int i = 0; i < height * width; i++)
+    {
+        for (int j = 0; j < clusters; j++)
+        {
+            if (pixel_cluster_mapping[i] == j)
+                for (int k = 0; k < channels; k++)
+                {
+                    matrix[i][k] = centroids[j][k];
+                }
         }
     }
 }
 
-KMeans::~KMeans() {
+KMeans::~KMeans()
+{
 
-    for(int i=0; i<clusters; i++) {
+    for (int i = 0; i < clusters; i++)
+    {
         delete[] centroids[i];
     }
     delete[] centroids;
--- test.cpp
+++ test.cpp
@@ -1,20 +1,27 @@
 #include <bits/stdc++.h>
+#include <chrono>
 #include "ImageRead.hpp"
 #include "ImageWrite.hpp"
 #include "KMeans.hpp"
 using namespace std;
 
-int main() {
-    ImageReader imread("image.png",3);
-    cout<<imread.width<<" "<<imread.height<<endl;
+int main()
+{
+    ImageReader imread("image.png", 3);
+    // cout<<imread.width<<" "<<imread.height<<endl;
     KMeans model(256, imread.matrix, imread.height, imread.width, imread.channels);
     int ITERATIONS = 20;
-    for(int i=0; i<ITERATIONS; i++) {
+    auto start = chrono::high_resolution_clock::now();
+    for (int i = 0; i < ITERATIONS; i++)
+    {
         model.computeClosestCentroid();
         model.computeCentroids();
     }
     model.assignCentroids();
+    auto stop = chrono::high_resolution_clock::now();
     ImageWriter imsave("image2.png", imread.width, imread.height, imread.channels, model.matrix);
     imsave.save();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    cout << duration.count()/pow(10,6) << "s to compress the image" << endl;
     return 0;
 }
